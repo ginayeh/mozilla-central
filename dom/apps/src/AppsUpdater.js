500802f409816e532fdc3cc0808989a8843c5090
Gina Yeh <gyeh@mozilla.com>
2012-07-31 15:52:03 +0800

    fabrice

--- dom/apps/src/AppsUpdater.js
+++ dom/apps/src/AppsUpdater.js
@@ -4,23 +4,36 @@
 
 "use strict";
 
-const Cu = Components.utils;
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
+var Cu = Components.utils;
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+var Cr = Components.results;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/Webapps.jsm");
+//Cu.import("resource://gre/modules/ApplicationCacheUpdateService.jsm");
 
 function AppsUpdater() {
+	this.timer = null;
+	this.init();
 }
  
 AppsUpdater.prototype = {
+	init: function init() {
+		this.timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+		this.timer.initWithCallback(this, 200, this.timer.TYPE_REPEATING_SLACK);
+	},
+
+	stop: function stop() {
+		this.timer.cancel();
+		this.timer = null;
+	},
  
 	// This function is called by the Update Timer Manager for us.
 	// We need to update packaged apps and apps that use the offline cache.
 	notify: function appsUpdater_notify(aTimer) {
+		dump("Get notified.");
 		// Don't do anything if we're offline.
 		// XXX : check that we're on wifi/wired/3G+ connection?
 		if (Services.io.offline) {
@@ -34,6 +47,9 @@ AppsUpdater.prototype = {
 				this.updatePackagedApp(apps[id]);
 			} else {
 				// TODO: test is this app manifest references an offline cache, and update it.
+					for (let prop in apps[id]) {
+						dump(prop + ": "+ apps[id][prop]);
+					}
 			}
 		}
 	},
