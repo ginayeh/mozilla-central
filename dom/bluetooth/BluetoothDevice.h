ae97dae47fc6baecad8412c9b833b6a849c56bae
Gina Yeh <gyeh@mozilla.com>
2012-08-16 10:16:27 +0800

    add onconnected evnet and ondisconnected event

--- dom/bluetooth/BluetoothDevice.h
+++ dom/bluetooth/BluetoothDevice.h
@@ -1,5 +1,5 @@
 /* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
-/* vim: set ts=2 et sw=2 tw=40: */
+/* vim: set ts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -75,6 +75,8 @@ private:
   nsTArray<nsString> mUuids;
 
   NS_DECL_EVENT_HANDLER(propertychanged)
+  NS_DECL_EVENT_HANDLER(connected)
+  NS_DECL_EVENT_HANDLER(disconnected)
 };
 
 END_BLUETOOTH_NAMESPACE
